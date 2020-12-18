c4589f1091603d53b639c21261233f2bbb6d227e
Alderano Fileni <73562784+afileni@users.noreply.github.com>
2020-12-18 20:32:56 -0300

    Update api.py

--- api2/api.py
+++ api2/api.py
@@ -48,7 +48,7 @@ db.create_all()
 @app.route('/products', methods = ['GET'])
 def index():
     get_products = Product.query.all()
-    product_schema = ProductSchema(many=True)
-    products = product_schema.dump(get_products)
+    #product_schema = ProductSchema(many=True)
+    #products = product_schema.dump(get_products)
     return make_response(jsonify({"product": products}))
     
