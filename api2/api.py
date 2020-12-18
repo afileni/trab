5e95e0733944b0af867011a925cbddd4fcc6cbe1
Alderano Fileni <73562784+afileni@users.noreply.github.com>
2020-12-18 18:41:15 -0300

    Update api.py

--- api2/api.py
+++ api2/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-#from marshmallow_sqlalchemy import ModelSchema
+from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
 
 app = Flask(__name__)
